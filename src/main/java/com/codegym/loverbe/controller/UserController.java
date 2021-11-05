78478d11edf266a29778e2a47ae32dc80e9dac76
nguyenthanhliem-ntl <76995783+nguyenthanhliem-ntl@users.noreply.github.com>
2021-11-05 14:38:33 +0700

    up find all user

--- src/main/java/com/codegym/loverbe/controller/UserController.java
+++ src/main/java/com/codegym/loverbe/controller/UserController.java
@@ -1,6 +1,17 @@
 package com.codegym.loverbe.controller;
 
+import com.codegym.loverbe.model.Supplier;
+import com.codegym.loverbe.model.User;
+import com.codegym.loverbe.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @CrossOrigin(origins = "*")
 public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping
+    public ResponseEntity<?> pageFindAll(@PageableDefault(sort = "username", direction = Sort.Direction.ASC) Pageable pageable){
+
+        Page<User> supplierPage = userService.pageFindAll(pageable);
+        if (supplierPage.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(supplierPage,HttpStatus.OK);
+        }
+    }
 
 }
